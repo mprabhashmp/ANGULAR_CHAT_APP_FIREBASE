e05b3315da12251e92df43a7f47b90e0a2991541
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-12-06 23:07:34 +0530

    modyfi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotpassswordComponent } from './forgotpasssword/forgotpasssword.component';
+import { EmailComponent } from './email/email.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo: '/login', pathMatch: 'full'},
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgotpassword', component: ForgotpassswordComponent },
+  { path: 'email', component: EmailComponent },
+  {path:'dashboard',component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
