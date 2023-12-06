7a7f3f389f5aaf023cce1bcdbf875634a0faa933
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-12-06 23:08:09 +0530

    environment

---
+++ src/app/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyDNmHiEESQrTmJkFzF-5SuENxmMUpl8ZiU",
+      authDomain: "musixcom-6fd4a.firebaseapp.com",
+      projectId: "musixcom-6fd4a",
+      storageBucket: "musixcom-6fd4a.appspot.com",
+      messagingSenderId: "815083176314",
+      appId: "1:815083176314:web:fc2b86046252eba7c2d6c3"
+    }
+  };
