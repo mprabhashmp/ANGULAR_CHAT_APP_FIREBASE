852895cdd2efa0b5c982e75dc19ac2400e450163
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-12-07 14:44:57 +0530

    services

---
+++ src/app/environment.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvironmentService {
+
+  // constructor() { }
+
+  // production!: false
+
+  // firebaseConfig!: {
+  //   apiKey: 'YOUR_API_KEY';
+  //   authDomain: 'YOUR_AUTH_DOMAIN';
+  //   projectId: 'YOUR_PROJECT_ID';
+  // };
+}
