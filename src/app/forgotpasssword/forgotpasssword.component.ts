397d4ee78d6bbb8ae2f05fdf838c26d0054e8793
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-12-23 18:32:22 +0530

    updated 23

--- src/app/forgotpasssword/forgotpasssword.component.ts
+++ src/app/forgotpasssword/forgotpasssword.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpasssword',
@@ -9,10 +10,34 @@ import { AuthServiceService } from '../auth-service.service';
 export class ForgotpassswordComponent implements OnInit{
 
   constructor(
-    public authService: AuthServiceService
+    public authService: AuthServiceService , private router:Router,
   ) { }
 
 
   ngOnInit() {
+  }
+
+
+  isLoading: boolean = false;
+  Forgotpass(email: string): void {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      // Redirect to login page after the delay
+      this.router.navigate(['/login']);
+    }, 3000); // 
+
+    // Call the AuthService method
+    this.authService.ForgotPassword(email)
+      .then(() => {
+        // Reset is successful, hide loading indicator
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        // Handle errors if necessary
+        console.error(error);
+        // Hide loading indicator even in case of an error
+        this.isLoading = false;
+      });
   }
 }
